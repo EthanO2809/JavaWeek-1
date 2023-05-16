c7dde78d89acb6943ee3e63158377b6bdb22931a
Ethan Oliver <ethanliamoliver28@gmail.com>
2023-05-16 09:33:13 +0200

    first commit

---
+++ Scripts/Index.js
@@ -0,0 +1,26 @@
+// Task 1
+const person= {
+    name: "Ethan",
+    Surname: "Oliver",
+    Occupation: "Web Developer",
+    Age: "21",
+    Gender: "Male",
+}
+
+let {name, Surname, Occupation, Age, Gender}= person
+
+console.log (name,Surname,Occupation,Age,Gender)
+
+// Task 2
+
+const person2= {
+    Name: 'John',
+    age: 30,
+    isUser: true,
+    firstName: undefined,
+    selectedColor: null,
+}
+
+let {Name, age, isUser, firstName, selectedColor}= person2
+
+console.log (Name,age,isUser,firstName,selectedColor)
